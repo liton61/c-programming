bf5b0de070c4ee2bf9f12c569af089fbe996c2ee
liton61 <litonofficial61@gmail.com>
2025-02-17 22:54:38 +0600

    problem 1 solved

---
+++ p-1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("Recently I heard that you’ve achieved 95% marks in your exam.\nThis is brilliant!\n I wish you’ll shine in your life!\t	Good luck with all the barriers(/\\) in your life.");
+    return 0;
+}
